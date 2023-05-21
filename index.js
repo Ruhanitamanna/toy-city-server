d467a879b4368be175ea508f4d8718fd27022b06
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-05-21 09:54:05 +0600

    cached .env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT ||5000;
@@ -19,8 +19,7 @@ app.get('/',(req,res)=>{
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lruiqni.mongodb.net/?retryWrites=true&w=majority`;
-console.log(process.env.DB_USER)
-console.log(process.env.DB_PASS)
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -43,6 +42,16 @@ async function run() {
         const cursor = toysCollection.find();
         const result = await cursor.toArray();
         res.send(result);
+    }) 
+
+
+    app.get('/toys/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        console.log('id:', id);
+        console.log('_id:', ObjectId);
+        const result = await toysCollection.findOne(query);
+        res.send(result)
     })
 
 
